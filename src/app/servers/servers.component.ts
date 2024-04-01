c1fd77d0144b2662faa25c3f87e8a290f2b8ee3b
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-01 15:35:43 +0530

    learned about styling and components creation by cli, components as tag, attribute, class, and there usage

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector as a tag
+  selector: 'app-servers',
+  // selector as a attribute
+  // selector: '[app-servers]',
+  // selector as a class
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
