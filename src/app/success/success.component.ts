298aa22850b36d05848a3681bc4686d58852ea6a
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-01 15:48:19 +0530

    success with inline template and styles in ts file and warning with css, html, and ts file different and generated by cli cmd

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-success',
+    template:`
+    <h1>Success</h1>
+    `,
+    styles:`
+    h1{
+        color:green;
+        padding:10px;
+    }
+    `
+})
+export class SuccessComponent {}
